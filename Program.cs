0112e0b17de884f163fe5bfb89825e637990f3e5
jon walker <timmyburner888@gmail.com>
2020-10-10 19:00:16 -0500

    Commit

--- Program.cs
+++ Program.cs
@@ -1,48 +1,48 @@
 ﻿using System;
 using System.IO;
 
-namespace Homework
+namespace TicketSystem
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string file = "courseData.txt";
+            string file = "ticket.txt";
             string choice;
             do
             {
-                // ask user a question
+                
                 Console.WriteLine("1) Read data from file.");
                 Console.WriteLine("2) Create file from data.");
                 Console.WriteLine("Enter any other key to exit.");
-                // input response
+                
                 choice = Console.ReadLine();
 
                 if (choice == "1")
                 {
-                    // read data from file
+                    
                     if (File.Exists(file))
                     {
-                        // accumulators needed for GPA
+                        
                         int gradePoints = 0;
                         int count = 0;
-                        // read data from file
+                        
                         StreamReader sr = new StreamReader(file);
                         while (!sr.EndOfStream)
                         {
                             string line = sr.ReadLine();
-                            // convert string to array
+                            
                             string[] arr = line.Split('|');
-                            // display array data
-                            Console.WriteLine("Course: {0}, Grade: {1}", arr[0], arr[1]);
-                            // add to accumulators
+                            
+                            Console.WriteLine("Name: {0}, Grade: {1}", arr[0], arr[1]);
+                            
                             gradePoints += arr[1] == "A" ? 4 : arr[1] == "B" ? 3 : arr[1] == "C" ? 2 : arr[1] == "D" ? 1 : 0;
                             count += 1;
                         }
                         sr.Close();
-                        // calculate GPA
+                        
                         double GPA = (double)gradePoints / count;
-                        Console.WriteLine("GPA: {0:n2}", GPA);
+                        Console.WriteLine("Ticket: {0:n2}", GPA);
                     }
                     else
                     {
@@ -51,23 +51,23 @@ namespace Homework
                 }
                 else if (choice == "2")
                 {
-                    // create file from data
+                    
                     StreamWriter sw = new StreamWriter(file);
                     for (int i = 0; i < 7; i++)
                     {
-                        // ask a question
-                        Console.WriteLine("Enter a course (Y/N)?");
-                        // input the response
+                        
+                        Console.WriteLine("Enter a Priority (Y/N)?");
+                        
                         string resp = Console.ReadLine().ToUpper();
-                        // if the response is anything other than "Y", stop asking
+                        
                         if (resp != "Y") { break; }
-                        // prompt for course name
-                        Console.WriteLine("Enter the course name.");
-                        // save the course name
+                       
+                        Console.WriteLine("Enter the customer name.");
+                    
                         string name = Console.ReadLine();
-                        // prompt for course grade
-                        Console.WriteLine("Enter the course grade.");
-                        // save the course grade
+                     
+                        Console.WriteLine("Enter the problem.");
+                   
                         string grade = Console.ReadLine().ToUpper();
                         sw.WriteLine("{0}|{1}", name, grade);
                     }
